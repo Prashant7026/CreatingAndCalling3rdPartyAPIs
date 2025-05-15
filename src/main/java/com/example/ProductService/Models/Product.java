7eec4a438736caaa9a470189b6ea1eaa85ca287d
Prashant7026 <prashantkumarsoni30@gmail.com>
2025-05-15 19:36:37 +0530

    Models

---
+++ src/main/java/com/example/ProductService/Models/Product.java
@@ -0,0 +1,18 @@
+package com.example.ProductService.Models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToOne;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+public class Product extends BaseModel {
+    private String title;
+    private String description;
+    private double price;
+    private String imageUrl;
+    @ManyToOne
+    private Category category;
+}
