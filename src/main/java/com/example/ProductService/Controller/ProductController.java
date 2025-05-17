3743a7851b0ce86d84c9ae09a7a76ff6f227c9b5
Prashant7026 <prashantkumarsoni30@gmail.com>
2025-05-17 13:07:36 +0530

    Create & get all product

--- src/main/java/com/example/ProductService/Controller/ProductController.java
+++ src/main/java/com/example/ProductService/Controller/ProductController.java
@@ -1,7 +1,37 @@
 package com.example.ProductService.Controller;
 
+import com.example.ProductService.DTO.Product.*;
+import com.example.ProductService.Models.Product;
+import com.example.ProductService.Service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
+@RequestMapping("/products")
 public class ProductController {
+
+    @Autowired
+    private ProductService productService;
+
+    @PostMapping("")
+    public CreateProductResponseDto createProduct(@RequestBody CreateProductRequestDto productRequestDto) {
+        Product product = productService.createProduct(productRequestDto.toProduct());
+        return CreateProductResponseDto.fromProduct(product);
+    }
+
+    @GetMapping("")
+    public List<CreateProductResponseDto> getAllProduct() {
+        List<CreateProductResponseDto> responseProductDto = new ArrayList<>();
+
+        List<Product> products = productService.findAll();
+        for (Product product : products) {
+            responseProductDto.add(CreateProductResponseDto.fromProduct(product));
+        }
+        return responseProductDto;
+    }
     @RequestMapping(name = "NAMAN", value = "/products")
     public String namanMethod() {
         return "Magic";
